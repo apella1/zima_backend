20701c9b6a454227f3d84286d713fb19a94f9dfa
apella1 <johncollons5@gmail.com>
2023-03-07 23:59:50 +0300

    Configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from "express";
+import express, { Express, Response, Request } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
 
-const app = express();
+dotenv.config();
+
+const app: Express = express();
 const url =
   "mongodb+srv://Reed:wFAUB3d1eliYbxJT@cluster0.cw2cigc.mongodb.net/?retryWrites=true&w=majority";
 
 const port = 3000;
 
-app.get("/", (_, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Zima backend is live!");
 });
 
